1f3f2ae2e8e6ba3a4e7542878632bdc288f4610c
James Felix Black <git@homonculus.net>
2020-12-08 17:15:36 -0500

    Day 8
    With vectors it goes fast!

--- 2020/src/Day08.hs
+++ 2020/src/Day08.hs
@@ -1,37 +1,60 @@
 module Day08 where
 
-testData = "nop +0\nacc +1\njmp +4\nacc +3\njmp -3\nacc -99\nacc +1\njmp -4\nacc +6"
+import Data.Vector (Vector, (!), (!?), (//))
+import qualified Data.Vector as V
 
-data Instruction = NOP Int | JMP Int Int | ACC Int Int deriving (Show, Eq)
+testStr :: String
+testStr = "nop +0\nacc +1\njmp +4\nacc +3\njmp -3\nacc -99\nacc +1\njmp -4\nacc +6"
 
-parseStr :: String -> [Instruction]
-parseStr = zipWith (curry parseLine) [0..] . lines
+testData :: Program
+testData = parseStr testStr
+
+data Instruction = NOP Int | JMP Int | ACC Int deriving (Show, Eq)
+data Result = Cycle Int | Termination Int deriving (Show, Eq)
+type Program = Vector Instruction
+
+
+parseStr :: String -> Program
+parseStr = V.fromList . map parseLine . lines
   where
-    parseLine (idx, s) =
-      case i of "nop" -> NOP idx
-                "jmp" -> JMP idx v
-                "acc" -> ACC idx v
+    parseLine s =
+      case i of "nop" -> NOP v
+                "jmp" -> JMP v
+                "acc" -> ACC v
                 _     -> error ("Invalid instruction " ++ s)
         where
           s' = words s
           i = head s'
           v = intMe (head . tail $ s')
-          intMe [] = error ("Invalid instruction " ++ s)
-          intMe s = (read (tail s) :: Int) * (if head s == '-' then (-1) else 1)
+          intMe str = (read (tail str) :: Int) * (if head str == '-' then (-1) else 1)
                                       
-part01 :: [Instruction] -> Int
-part01 instructions =
-  go 0 [] (head instructions)
+run :: Program -> Result
+run instructions =
+  go 0 [] 0
   where
-    go acc seen i | i `elem` seen = acc
+    go acc seen i | i `elem` seen = Cycle acc
+    go acc _    i | i >= V.length instructions = Termination acc
     go acc seen i =
-      case i of
-        NOP idx   -> go acc       (seen ++ [i]) (instructions !! (idx + 1))
-        ACC idx v -> go (acc + v) (seen ++ [i]) (instructions !! (idx + 1))
-        JMP idx v -> go acc       (seen ++ [i]) (instructions !! (idx + v))
+      case instructions ! i of
+        NOP _ -> go acc       (seen ++ [i]) (i + 1)
+        ACC v -> go (acc + v) (seen ++ [i]) (i + 1)
+        JMP v -> go acc       (seen ++ [i]) (i + v)
+
+part01 :: Program -> Result
+part01 = run
+
+part02 :: Program -> Result
+part02 program = head . filter isOk . map run . zipWith (\idx istr -> program // [(idx, switch istr)]) [0..] . V.toList $ program 
+  where
+    isOk (Termination _) = True
+    isOk _             = False
+    switch (NOP i) = JMP i
+    switch (JMP i) = NOP i
+    switch i       = i 
 
 solution :: IO ()
 solution = do
   instrs <- parseStr <$> readFile "data/Day08.txt"
   print (part01 instrs)
+  print (part02 instrs)     
   
