0c95dc24c4be95ea48db19a2af3d079ba879a4db
James Felix Black <git@homonculus.net>
2020-12-16 16:50:23 -0500

    Day 13
    part 1, part 2 is nasty

---
+++ 2020/src/Day13.hs
@@ -0,0 +1,34 @@
+module Day13 where
+
+import Data.List.Split (splitOn)
+import Text.Read (readMaybe)
+import Data.Maybe (mapMaybe)
+import Data.Sort (sortOn)
+
+testStr = "939\n7,13,x,x,59,x,31,19"
+
+-- parseInput :: String -> (Int, [Int])
+parseInput s = (start, sortOn snd . map toRange . mapMaybe readMaybe . splitOn "," $ xs)
+  where [x,xs] = lines s
+        start = read x :: Int
+        toRange p = head $ [(p, i) | i <- [0,p..], i > start]
+
+solve01 :: String -> Int
+solve01 s =
+  let
+    (start, ps) = parseInput s
+    (bus, m) = head ps
+  in
+    bus * (m - start)
+-- solve01 :: Int -> [Maybe Int] -> Int
+-- solve01 start =
+--   routeNum * earliestTime
+--   where
+--     map (\i -> head $ [(x, i) | x <- [0,i..], x > start])
+    
+
+ 
+solution = do
+  s <- readFile "data/Day13.txt"
+  print (solve01 s)
+  
